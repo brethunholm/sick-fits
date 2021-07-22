cfb5c755deb1467c2faa8ea37a86e2f66b993322
Bre Thunholm <bthunholm@gmail.com>
2021-07-21 18:30:48 -1000

    updated app

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "6d2c135f5538d781e520";
+/******/ 	var hotCurrentHash = "450873736639f192bdc4";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
